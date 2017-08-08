7520af190fcf01ae9298ab53c524df34a60e7993
Noah Kang <noahskang@gmail.com>
2017-08-08 16:37:14 -0700

    Added axios reader

--- transitninja/index.ios.js
+++ transitninja/index.ios.js
@@ -11,43 +11,39 @@ import {
   Text,
   View
 } from 'react-native';
+import Header from './src/components/Header';
+// import Map from './src/components/Map';
+import TemporaryConnection from './src/components/TemporaryConnection';
 
 export default class transitninja extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <Header headerText={'Search Here'} / >
+        <TemporaryConnection />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// <Map />
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
 
 AppRegistry.registerComponent('transitninja', () => transitninja);
---
+++ transitninja/src/components/TemporaryConnection.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import axios from 'axios';
+
+class TemporaryConnection extends Component {
+  constructor() {
+    super();
+    this.state = { transit_info: [] };
+  }
+  componentWillMount() {
+    // We need to replace the following url with the location of our server. (e.g. replace with http://localhost:3000)
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({ transit_info: response.data }));
+      // http requests return a promise to us, because http requests are inherently asynchronous.
+
+  }
+
+
+  render() {
+    console.log("state", this.state.transit_info);
+
+    return (
+      <View>
+      <Text>
+        This is a temporary filler. Check the console logs to see if the json information from oru backend is being returned correctly.
+      </Text>
+      </View>
+    );
+  }
+
+}
+export default TemporaryConnection;
